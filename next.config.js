0f864927847b4da4cf80227a897ff118c157e61b
moncef2006 <73278962+moncef2006@users.noreply.github.com>
2022-11-06 17:46:38 +0100

    fix configue

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "example.com",
-        port: "",
-        pathname: "/account123/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
